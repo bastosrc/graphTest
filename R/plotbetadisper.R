642edd739fd6bbb55d8cc6afa97c480672ffae43
bastosrc <60294023+bastosrc@users.noreply.github.com>
2021-08-22 20:05:19 -0300

    Update plotbetadisper.R

--- R/plotbetadisper.R
+++ R/plotbetadisper.R
@@ -42,18 +42,18 @@ plot1<-ggplot(ord, aes(x=PCoA1, y=PCoA2, colour=ord[,3]))+
 
 if(length(levels(ord2[,3]))==2){
 plot1<-plot1+geom_segment(data=trat1, aes(x=x$centroids[1], y=x$centroids[3], xend=PCoA1, yend=PCoA2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="gray")+
-	annotate("text", x=x$centroids[1], y=x$centroids[3], label=levels(ord[,3])[1], family="serif", size=5)+
+	annotate("text", x=x$centroids[1], y=x$centroids[3], label=levels(ord[,3])[1], family="serif", size=6)+
 	geom_segment(data=trat2, aes(x=x$centroids[2], y=x$centroids[4], xend=PCoA1, yend=PCoA2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="gray")+
-	annotate("text", x=x$centroids[2], y=x$centroids[4], label=levels(ord[,3])[2], family="serif", size=5)+
+	annotate("text", x=x$centroids[2], y=x$centroids[4], label=levels(ord[,3])[2], family="serif", size=6)+
 	geom_point(shape=2, size=3)
 }
 if(length(levels(ord2[,3]))==3){
 plot1<-plot1+geom_segment(data=trat1, aes(x=x$centroids[1], y=x$centroids[4], xend=PCoA1, yend=PCoA2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="gray")+
-	annotate("text", x=x$centroids[1], y=x$centroids[4], label=levels(ord[,3])[1], family="serif", size=5)+
+	annotate("text", x=x$centroids[1], y=x$centroids[4], label=levels(ord[,3])[1], family="serif", size=6)+
 	geom_segment(data=trat2, aes(x=x$centroids[2], y=x$centroids[5], xend=PCoA1, yend=PCoA2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="gray")+
-	annotate("text", x=x$centroids[2], y=x$centroids[5], label=levels(ord[,3])[2], family="serif", size=5)+
+	annotate("text", x=x$centroids[2], y=x$centroids[5], label=levels(ord[,3])[2], family="serif", size=6)+
 	geom_segment(data=trat3, aes(x=x$centroids[3], y=x$centroids[6], xend=PCoA1, yend=PCoA2), arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="gray")+
-	annotate("text", x=x$centroids[3], y=x$centroids[6], label=levels(ord[,3])[3], family="serif", size=5)+
+	annotate("text", x=x$centroids[3], y=x$centroids[6], label=levels(ord[,3])[3], family="serif", size=6)+
 	geom_point(shape=2, size=3)
 }
 return(plot1)
